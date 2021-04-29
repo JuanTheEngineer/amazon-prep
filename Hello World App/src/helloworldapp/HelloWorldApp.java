64499e9a6752637694821ef39ad3f55dfb0d9d00
JuanTheEngineer <juanrodriguezjesus@gmail.com>
2021-04-29 13:39:01 -0400

    Adding first two projects to repo

---
+++ Hello World App/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * If you want to rename the node for a source file in the Projects window, 
+ * choose Refactor from IDE's main menu. The IDE prompts you with the Rename 
+ * dialog box to lead you through the options of renaming the class and the 
+ * updating of code that refers to that class. Make the changes and 
+ * click Refactor to apply the changes. This sequence of clicks might seem 
+ * unnecessary if you have just a single class in your project, but it is very 
+ * useful when your changes affect other parts of your code in larger projects.
+ */
+package helloworldapp; 
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, World!");    // Display the String
+    }
+    
+}
